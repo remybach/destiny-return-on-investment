82045b9a161f98a27c91f1d529feec8529271e48
Remy Bach <remy@bach.me.uk>
2017-04-21 12:42:31 +0100

    Update wording on comparisons slightly.

--- app/Comparisons.jsx
+++ app/Comparisons.jsx
@@ -50,7 +50,7 @@ class Comparisons extends React.Component {
     let comparisonText;
 
     if (hasComparisons) {
-      comparisonText = `${ this.props.comparisons[this.state.currentComparison].title } would provide approximately ${ this.props.comparisons[this.state.currentComparison].approximateCostPerHour } worth of entertainment per hour.`;
+      comparisonText = `${ this.props.comparisons[this.state.currentComparison].title }, you spend ${ this.props.comparisons[this.state.currentComparison].approximateCostPerHour } to be entertained per hour.`;
 
       if (this.props.comparisons.length > 1) {
         refreshIcon = (<ActionCached className="refresh-icon" onClick={ this.refreshComparison.bind(this) } />);
